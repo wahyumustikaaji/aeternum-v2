35d55a46265a7f824dfe3d04ce440ce499fbfe14
WayuAji30 <wahyuma123@gmail.com>
2025-05-13 15:00:10 +0700

    add: section solution at home

--- src/components/home/SolutionSection.jsx
+++ src/components/home/SolutionSection.jsx
@@ -4,7 +4,7 @@ export default function SolutionSection() {
         <div className="mx-auto max-w-2xl lg:max-w-7xl">
           <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2">
             <div className="relative lg:row-span-2">
-              <div className="absolute inset-px rounded-lg bg-[#171717] lg:rounded-l-[2rem]"></div>
+              <div className="absolute inset-px rounded-lg bg-[#171717] lg:rounded-l-[2rem] border"></div>
               <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
                 <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
                   <p className="mt-2 text-lg font-medium tracking-tight text-white max-lg:text-center">
@@ -19,7 +19,7 @@ export default function SolutionSection() {
                     <img
                       className="size-full object-cover object-top ml-0.5"
                       src="/assets/images/instan-verification.webp"
-                      alt=""
+                      alt="instan-verification"
                     />
                   </div>
                 </div>
@@ -27,7 +27,7 @@ export default function SolutionSection() {
               <div className="pointer-events-none absolute inset-px rounded-lg shadow-sm ring-1 ring-black/5 lg:rounded-l-[2rem]"></div>
             </div>
             <div className="relative max-lg:row-start-1">
-              <div className="absolute inset-px rounded-lg bg-[#171717] max-lg:rounded-t-[2rem]"></div>
+              <div className="absolute inset-px rounded-lg bg-[#171717] max-lg:rounded-t-[2rem] border"></div>
               <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
                 <div className="px-8 pt-8 sm:px-10 sm:pt-10">
                   <p className="mt-2 text-lg font-medium tracking-tight text-white max-lg:text-center">Trusted Digital Ecosystem</p>
@@ -38,15 +38,15 @@ export default function SolutionSection() {
                 <div className="flex flex-1 items-center justify-center px-8 max-lg:pt-10 max-lg:pb-12 sm:px-10 lg:pb-2">
                   <img
                     className="w-full max-lg:max-w-xs"
-                    src="https://tailwindcss.com/plus-assets/img/component-images/bento-03-performance.png"
-                    alt=""
+                    src="/assets/images/ecosystem.png"
+                    alt="ecosystem"
                   />
                 </div>
               </div>
               <div className="pointer-events-none absolute inset-px rounded-lg shadow-sm ring-1 ring-black/5 max-lg:rounded-t-[2rem]"></div>
             </div>
             <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
-              <div className="absolute inset-px rounded-lg bg-[#171717]"></div>
+              <div className="absolute inset-px rounded-lg bg-[#171717] border"></div>
               <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)]">
                 <div className="px-8 pt-8 sm:px-10 sm:pt-10">
                   <p className="mt-2 text-lg font-medium tracking-tight text-white max-lg:text-center">Secure & Immutable</p>
@@ -57,15 +57,15 @@ export default function SolutionSection() {
                 <div className="@container flex flex-1 items-center max-lg:py-6 lg:pb-2">
                   <img
                     className="h-[min(152px,40cqw)] object-cover"
-                    src="https://tailwindcss.com/plus-assets/img/component-images/bento-03-security.png"
-                    alt=""
+                    src="/assets/images/security.png"
+                    alt="security"
                   />
                 </div>
               </div>
               <div className="pointer-events-none absolute inset-px rounded-lg shadow-sm ring-1 ring-black/5"></div>
             </div>
             <div className="relative lg:row-span-2">
-              <div className="absolute inset-px rounded-lg bg-[#171717] max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
+              <div className="absolute inset-px rounded-lg bg-[#171717] max-lg:rounded-b-[2rem] lg:rounded-r-[2rem] border"></div>
               <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
                 <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
                   <p className="mt-2 text-lg font-medium tracking-tight text-white max-lg:text-center">
@@ -80,7 +80,7 @@ export default function SolutionSection() {
                     <img
                       className="size-full object-cover object-top ml-0.5"
                       src="/assets/images/share.webp"
-                      alt=""
+                      alt="share"
                     />
                   </div>
                 </div>
